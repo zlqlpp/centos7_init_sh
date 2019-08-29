25f931ffe2953a6e8aaef885c50006259641af0d
aqnd <zlqlpp@gmail.com>
2019-08-29 09:53:05 +0800

    1
    1

--- centos7_init.sh
+++ centos7_init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-yum install -y wget curl vim sed tsar  epel-release
+yum install -y wget curl vim sed tsar  epel-release; #安装下载，文本处理，系统监控，第三方源
+
+service crond start; 
+
+setenforce 0;
