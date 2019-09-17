33dc2b986f38d3866d052e4376ecc6fe90328e40
aqnd <zlqlpp@gmail.com>
2019-09-17 21:19:12 +0800

    unzip

--- centos7_init.sh
+++ centos7_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y wget curl vim sed tsar  epel-release; #安装下载，文本处理，系统监控，第三方源
+yum install -y wget curl vim sed tsar  epel-release unzip; #安装下载，文本处理，系统监控，第三方源
 
 service crond start; 
 
