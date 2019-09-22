4ef974ef19112214563414e3438c9e7a8f11a39f
aqnd <zlqlpp@gmail.com>
2019-09-22 21:23:53 +0800

    增加注释

--- centos7_init.sh
+++ centos7_init.sh
@@ -2,8 +2,8 @@
 
 yum install -y wget curl vim sed tsar  epel-release unzip; #安装下载，文本处理，系统监控，第三方源
 
-yum groupinstall 'Development Tools' -y ;
+yum groupinstall 'Development Tools' -y ;                  #安装开发工具
 
-service crond start; 
+service crond start;                                       #启动crontable
 
-setenforce 0;
+setenforce 0;                                              #关闭  selinux
