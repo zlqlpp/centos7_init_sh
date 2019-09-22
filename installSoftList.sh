081b857f3b4f48b56197c8af69c3703cc7493871
aqnd <zlqlpp@gmail.com>
2019-09-22 22:34:30 +0800

    1

--- installSoftList.sh
+++ installSoftList.sh
@@ -28,3 +28,28 @@ case $no in
 	echo 'wrong'
 	
 esac
+
+
+#echo $LANG可以查看当前使用的系统语言
+#locale命令，如有zh cn 表示已经安装了中文语言
+#
+#查看系当前语言包 locale
+#查看系统拥有语言包 locale -a
+#安装简体中文语言包 yum install kde-l10n-Chinese
+#
+#
+#Centos7是Centos系列的最新版本，与老版本出现了较大操作差异，下面是安装中文支持
+#
+####中文支持
+#
+#安装语言包
+#
+#yum install kde-l10n-Chinese -y
+#修改系统默认语言
+#
+#localectl set-locale LANG=zh_CN.utf8
+#安装中文字体
+#
+#yum install ibus-table-chinese*
+#
+#重新连接终端即可
