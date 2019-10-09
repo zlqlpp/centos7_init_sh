7d4c40b911bf071b8be9e898c7be47614be444cd
zlqlpp <zlqlpp@gmail.com>
2019-10-09 11:45:36 +0800

    \

--- installSoftList.sh
+++ installSoftList.sh
@@ -4,7 +4,7 @@ printhelp(){
 	echo '########################################'
 	echo 'init 安装wget vim sed epel-release gcc等'
 	echo 'help 显示帮助'
-	
+	echo 'chn 安装中文字体,安装完成请重连'
 	
 	echo '########################################'
 }
@@ -16,11 +16,31 @@ init(){
 	service crond start;                                       #启动crontable
 	setenforce 0;                                              #关闭  selinux
 }
+##安装中文
+chn(){
+	#echo $LANG可以查看当前使用的系统语言
+	#locale命令，如有zh cn 表示已经安装了中文语言
+	#
+	#查看系当前语言包 locale
+	#查看系统拥有语言包 locale -a
+	#安装简体中文语言包 yum install kde-l10n-Chinese
+	#Centos7是Centos系列的最新版本，与老版本出现了较大操作差异，下面是安装中文支持
+	####中文支持
+	#安装语言包
+	yum install kde-l10n-Chinese -y
+	#修改系统默认语言
+	#
+	localectl set-locale LANG=zh_CN.utf8
+	#安装中文字体
+	yum install ibus-table-chinese*
+	#重新连接终端即可
 
+}
 ##读取用户输入
 readinput(){
 
-	read -p "enter number:" no
+	printhelp
+	read -p "enter commond:" no
 	#read -p "enter name:" name
 	#echo you have entered $no, $name
 
@@ -30,7 +50,7 @@ readinput(){
 		init 
 		;;
 		help)
-		echo 'printhelps'
+		echo 'printhelp'
 		;;
 		*)
 		echo 'wrong'
@@ -39,29 +59,7 @@ readinput(){
 
 }
 
-printhelp
+
 readinput
 
-#echo $LANG可以查看当前使用的系统语言
-#locale命令，如有zh cn 表示已经安装了中文语言
-#
-#查看系当前语言包 locale
-#查看系统拥有语言包 locale -a
-#安装简体中文语言包 yum install kde-l10n-Chinese
-#
-#
-#Centos7是Centos系列的最新版本，与老版本出现了较大操作差异，下面是安装中文支持
-#
-####中文支持
-#
-#安装语言包
-#
-#yum install kde-l10n-Chinese -y
-#修改系统默认语言
-#
-#localectl set-locale LANG=zh_CN.utf8
-#安装中文字体
-#
-#yum install ibus-table-chinese*
-#
-#重新连接终端即可
+
