d839c7b69ee4d6e969f6c31e7b22829d6da1acfb
zlqlpp <zlqlpp@gmail.com>
2019-10-09 11:58:03 +0800

    \

--- installSoftList.sh
+++ installSoftList.sh
@@ -1,17 +1,84 @@
 #! /bin/bash
 
+####################打印帮助
 printhelp(){
 	echo '########################################'
 	echo 'init 安装wget vim sed epel-release gcc等'
 	echo 'help 显示帮助'
 	echo 'chn 安装中文字体,安装完成请重连'
-	
+	echo 'ss 安装ss-server服务端,默认启动端口1521，密码123456,加密aes-256-gcm'
 	echo '########################################'
 	
 	readinput
 }
 
-##开发套件
+ss(){
+#1.删除旧版本依赖库
+rm -rf libsodium-* mbedtls-* simple-obfs shadowsocks-libev
+#2.安装依赖包
+yum install epel-release git gcc vim -y
+yum install -y wget gettext autoconf libtool automake make asciidoc xmlto zlib-devel libev-devel c-ares-devel pcre-devel libsodium-devel mbedtls-devel
+                                                                 
+##########################################################因为在Yum安安装了，所以不了再编译安装
+#5.编译依赖库 加密库
+wget -O libsodium-stable.tar.gz https://download.libsodium.org/libsodium/releases/LATEST.tar.gz;
+tar xzf libsodium-stable.tar.gz;
+pushd libsodium-stable/;
+./configure --prefix=/usr && make ;
+make install;
+popd;
+ldconfig;
+rm -rf libsodium-*;
+#6.安装依赖包2
+export MBEDTLS_VER=2.13.0;
+wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz;
+tar xzf mbedtls-$MBEDTLS_VER-gpl.tgz;
+pushd mbedtls-$MBEDTLS_VER;
+make SHARED=1 CFLAGS=-fPIC ;
+make DESTDIR=/usr install;
+popd;
+ldconfig;
+rm -rf mbedtls-*;
+#8. mbed
+#wget https://tls.mbed.org/download/mbedtls-2.5.1-apache.tgz;
+#tar zxvf mbedtls-2.5.1-apache.tgz;
+#cd mbedtls-2.5.1;
+#make; make install
+############################################################
+#3.安装依赖包 混淆(可以不安装)
+git clone --recursive https://github.com/shadowsocks/simple-obfs.git;
+pushd simple-obfs;
+./autogen.sh;
+./configure --prefix=/usr && make ;
+make install;
+popd;
+rm -rf simple-*;
+
+#4 shadowsocks-libev 服务端软件
+ git clone --recursive https://github.com/shadowsocks/shadowsocks-libev.git;
+pushd shadowsocks-libev;
+./autogen.sh;
+./configure --prefix=/usr && make ;
+make install;
+rm -rf shadowsocks-;
+#5 shadowsocks-libev 配置文件
+mkdir /etc/shadowsocks;
+cat <<-EOF > /etc/shadowsocks/ss.json
+{
+        "server":"0.0.0.0",
+        "server_port":1521,
+        "local_port":1080,
+        "password":"123456",
+        "timeout":60,
+        "method":"aes-256-gcm"
+}
+EOF
+
+#6.启动
+  setsid ss-server -c /etc/shadowsocks/ss.json -u &
+
+}
+##################开发套件
 init(){
 	yum install -y wget curl vim sed tsar  epel-release unzip; #安装下载，文本处理，系统监控，第三方源
 	yum groupinstall 'Development Tools' -y ;                  #安装开发工具
@@ -20,7 +87,7 @@ init(){
 	
 	readinput
 }
-##安装中文
+################安装中文
 chn(){
 	#echo $LANG可以查看当前使用的系统语言
 	#locale命令，如有zh cn 表示已经安装了中文语言
@@ -41,14 +108,14 @@ chn(){
 	readinput
 
 }
+
+
 ##读取用户输入
 readinput(){
 
-	
 	read -p "enter commond:" no
 	#read -p "enter name:" name
 	#echo you have entered $no, $name
-
 
 	case $no in 
 		init)
