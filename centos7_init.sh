f261cff3e29642542f96750afc7a029f54fc3167
aqnd <zlqlpp@gmail.com>
2019-10-18 22:25:25 +0800

    add git install

--- centos7_init.sh
+++ centos7_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y wget curl vim sed tsar  epel-release unzip; #安装下载，文本处理，系统监控，第三方源
+yum install -y wget curl vim sed tsar  epel-release unzip git; #安装下载，文本处理，系统监控，第三方源
 
 yum groupinstall 'Development Tools' -y ;                  #安装开发工具
 
