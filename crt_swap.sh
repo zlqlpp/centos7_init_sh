1036fd1f577f54537dab1fb2564e69b07d6a1937
aqnd <zlqlpp@gmail.com>
2019-12-12 11:43:00 +0800

    已整合到centos7.init.sh中

--- crt_swap.sh
+++
@@ -1,7 +0,0 @@
-dd if=/dev/zero of=/swapfile bs=1024 count=1M
-mkswap /swapfile
-swapon /swapfile
-swapon -s
-echo '/swapfile swap swap defaults 0 0' >>/etc/fstab
-chown root:root /swapfile; chmod 0600 /swapfile
-echo 'vm.swappiness=10' >>  /etc/sysctl.conf
