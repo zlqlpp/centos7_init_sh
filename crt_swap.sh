d3db03ac20bfefd9b8bcf0b02470a653de95d7ce
aqnd <zlqlpp@gmail.com>
2019-12-12 11:07:49 +0800

    创建1Gb的swap交换分区

--- crt_swap.sh
+++ crt_swap.sh
@@ -1,20 +1,7 @@
-swapon -s
-
 dd if=/dev/zero of=/swapfile bs=1024 count=1M
-
 mkswap /swapfile
-
 swapon /swapfile
-
 swapon -s
-
-vim /etc/fstab
-在最后一行添加上下面一条：
-    /swapfile     swap     swap     defaults     0  0
-	
- chown root:root /swapfile
-    chmod 0600 /swapfile
-
-/etc/sysctl.conf文件里添加如下参数：vm.swappiness=10
-
-保存，重启。
+echo '/swapfile swap swap defaults 0 0' >>/etc/fstab
+chown root:root /swapfile; chmod 0600 /swapfile
+echo 'vm.swappiness=10' >>  /etc/sysctl.conf
