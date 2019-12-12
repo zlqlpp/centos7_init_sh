11f4d4b269a7105133dae7cfac84131414fbcf39
aqnd <zlqlpp@gmail.com>
2019-12-12 11:25:44 +0800

    a

--- installSoftList.sh
+++ installSoftList.sh
@@ -164,8 +164,9 @@ cd ../;rm -rf shadowsocks-libev;
 }
 ##################开发套件
 init(){
-	yum install -y wget curl vim sed tsar  epel-release unzip; #安装下载，文本处理，系统监控，第三方源
+ 	yum install -y wget curl vim sed tsar  epel-release unzip git; #安装下载，文本处理，系统监控，第三方源
 	yum groupinstall 'Development Tools' -y ;                  #安装开发工具
+	
 	service crond start;                                       #启动crontable
 	setenforce 0;                                              #关闭  selinux
 	
