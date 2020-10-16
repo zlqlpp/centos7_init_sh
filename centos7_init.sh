db6bbde802fd9c720daff7e8806ab3e3009236a2
aqnd <zlqlpp@gmail.com>
2020-10-16 08:51:42 +0800

    Update centos7_init.sh
    增加 yum install sysstat （iostat）

--- centos7_init.sh
+++ centos7_init.sh
@@ -29,7 +29,7 @@ printhelp(){
 }
 ##################----a
 init(){
- 	yum install -y wget curl vim sed tsar  epel-release unzip git; #安装下载，文本处理，系统监控，第三方源
+ 	yum install -y wget curl vim sed tsar  epel-release unzip git sysstat; #安装下载，文本处理，系统监控，第三方源
 	yum groupinstall 'Development Tools' -y ;                  #安装开发工具
 	
 	service crond start;                                       #启动crontable
